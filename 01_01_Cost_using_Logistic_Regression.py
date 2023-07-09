35f5f56207fcb75075b73871b96f4b8578870017
Sambit K Satapathy <123438716+SambitSatapathy9@users.noreply.github.com>
2023-07-09 10:39:30 +0530

    Update 01_01_Cost_using_Logistic_Regression.py

--- 01_01_Cost_using_Logistic_Regression.py
+++ 01_01_Cost_using_Logistic_Regression.py
@@ -30,3 +30,31 @@ def plot_data(X,y,ax,pos_label = "y=1",neg_label = "y=0",loc="best",s=80):
     plt.show()
   
 plot_data(X_train,y_train,ax)
+
+#Defining the sigmoid function
+def sigmoid(z):
+    g = 1/(1+np.exp(-z))
+    return g
+
+#Defining the cost function
+def cost_logistic(X,y,w,b):
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w)+b
+        f_wb_i = sigmoid(z_i)
+        cost += -y[i]*np.log(f_wb_i)-(1-y[i])*np.log(1-f_wb_i)
+    cost = cost/m
+    return cost
+
+w_tmp = np.array([1,1]) #w1,w2,w3
+b_tmp = -4
+print(cost_logistic(X_train,y_train,w_tmp,b_tmp))
+
+#Decision Boundary
+
+
+
+
+
+
